554d99384adf9c4069ef91bef462c1d4ff3ac5ae
Fisnik58 <tocifisnik58@gmail.com>
2023-07-27 13:31:49 +0200

    remvedeverythingfromloading

--- pages/LoadingScreen.js
+++ pages/LoadingScreen.js
@@ -4,8 +4,7 @@ import React from "react";
 const LoadingScreen = () => {
   return (
     <div className="loading-screen">
-      {/* Add your loading screen content here */}
-      <h1>Loading...</h1>
+     
     </div>
   );
 };
