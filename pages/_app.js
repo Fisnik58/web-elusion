8b9790de9e5299cf1154a4b008efa5c2d50df2f2
Fisnik58 <tocifisnik58@gmail.com>
2023-07-27 10:17:11 +0200

    addednewestloadingscreen

--- pages/_app.js
+++ pages/_app.js
@@ -14,9 +14,9 @@ import 'animate.css';
 
 function LoadingScreen() {
   return (
-    <div className="loading-screen d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
-    <img src="/ElusionLogo.png"/>
-  </div>
+    <div class="loading-screen">
+        <div class="spinner"></div>
+    </div>
   
   );
 }
@@ -144,11 +144,11 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
-    {/* {isLoading ? (
+    {isLoading ? (
       <LoadingScreen />
     ) : (
       <div>
-        { */}
+        {
           <div className="whole-container" style={{ backgroundColor: "" }} id="home">
           <title>Elusion</title>
           <nav
@@ -681,9 +681,9 @@ onClick={() => setIsNavbarOpen((prevState) => !prevState)}
 
         </div>
      
-        {/* }
+         }
       </div>
-    )} */}
+    )} 
   </>
   );
 }
